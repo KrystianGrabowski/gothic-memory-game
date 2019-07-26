8a0ecddfe5181d536a4de97c470c536ab4062f75
KrystianGrabowski <krystian987543@gmail.com>
2019-07-26 17:55:14 +0200

    Images repleaceing

--- memory.js
+++ memory.js
@@ -1,17 +1,33 @@
 var cardNames = ["diego.jpg", "xardas.jpg", "diego.jpg", "gorn.jpg", "milten.jpg", "lares.jpg", "gorn.jpg", "xardas.jpg", "milten.jpg", "lester.jpg", "lester.jpg", "lares.jpg"]
+var secondCard = false;
+var prevCard = 0;
 
-function showCard(i){
-    $("#card" + i).css("background-image", "url('images/" + cardNames[i] + "')");
+function showCard(cardId){
+    $("#card" + cardId).css("background-image", "url('images/" + cardNames[cardId] + "')");
+    $("#card" + cardId).toggleClass("cardOnOff");
 }
 
 function prepareCards(){ 
     for(let i=0; i<12; i++){
-        $("<div id='card" + i + "' class='card'></div>")
-            .on("click", function(){showCard(i)})
+        $("<div id='card" + i + "'></div>")
+            .addClass("card")
+            .addClass("cardOnOff")
+            .on("click", function(){cardManager(i)})
             .appendTo("#board");
     }
 }
 
+function cardManager(cardId){
+    showCard(cardId);
+    if (secondCard){
+        secondCard = false;
+        alert(prevCard + ", " + cardId);
+    }
+    else{
+        prevCard = cardId;
+        secondCard = true;
+    }
+}
 
 
 prepareCards();
