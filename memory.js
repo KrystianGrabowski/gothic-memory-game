19f9c43450c844fd4283e30bca9cea062ce7d1ce
KrystianGrabowski <krystian987543@gmail.com>
2019-07-26 16:07:40 +0200

    Add cards

--- memory.js
+++ memory.js
@@ -1,7 +1,17 @@
+var cardNames = ["diego.jpg", "xardas.jpg", "diego.jpg", "gorn.jpg", "milten.jpg", "lares.jpg", "gorn.jpg", "xardas.jpg", "milten.jpg", "lester.jpg", "lester.jpg", "lares.jpg"]
+
+function showCard(i){
+    $("#card" + i).css("background-image", "url('images/" + cardNames[i] + "')");
+}
+
 function prepareCards(){ 
-    for(i=0; i<12; i++){
-        $("#board").append("<div class='card'></div>"); 
+    for(let i=0; i<12; i++){
+        $("<div id='card" + i + "' class='card'></div>")
+            .on("click", function(){showCard(i)})
+            .appendTo("#board");
     }
 }
+
+
 
 prepareCards();
