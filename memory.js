32a6f6fb5532c7949d9c7ca7045ae599b2ec7553
KrystianGrabowski <krystian987543@gmail.com>
2019-07-26 18:17:29 +0200

    Detect result and hide appropriate cards

--- memory.js
+++ memory.js
@@ -17,11 +17,33 @@ function prepareCards(){
     }
 }
 
+function compareCards(cardId1, cardId2){
+    return cardNames[cardId1] == cardNames[cardId2];
+}
+
+function updateScore(cardId1, cardId2){
+    if (compareCards(cardId1, cardId2)){
+        hideTwoCards(cardId1, cardId2);
+    }
+    else{
+        //miss
+    }
+}
+
+function hideTwoCards(cardId1, cardId2){
+    hideCard(cardId1);
+    hideCard(cardId2);
+}
+
+function hideCard(cardId){
+    $("#card" + cardId).css("opacity" , 0);
+}
+
 function cardManager(cardId){
     showCard(cardId);
     if (secondCard){
         secondCard = false;
-        alert(prevCard + ", " + cardId);
+        updateScore(prevCard, cardId);
     }
     else{
         prevCard = cardId;
